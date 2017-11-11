45b8d97fb8ad7ecaca6e28395ff7f152648ed8f1
Jennifer Stokes <jenniferstokes@Jennifers-MacBook-Pro.local>
2017-11-10 20:39:00 -0800

    partial working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,16 +6,47 @@ var computerChoices = ["a", "b", "c", "d", "e", "f",
 						"s", "t", "u", "v", "w", "x",
 						"y", "z",]
 
+var userChoices = [];			
+
 //just declaring some variables//
 var countDown = 10;
 var counter = 0;
 var start = computerChoices;
+var targetNum = 0;
+var winGame = 5;
+
 
 //this function is run whenever the user presses a key//
 document.onkeyup = function(event) {
 
 	//determines which key the user pressed//
 	var userGuess = event.key;
+	
+	
 
 	console.log(userGuess)
-}
+
+	//computer randomly chooses a letter from the array//
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices)]
+
+	//user guess conditions//
+	if (userGuess === computerGuess) {
+		counter +=1;
+		document.getElementById("wins").innerHTML = counter;
+		
+
+	
+	} else (userGuess != computerGuess) 
+		document.getElementById("letters").innerHTML = userGuess;
+		countDown -=1;
+		document.getElementById("left").innerHTML = countDown;
+
+	}
+	if (countDown === targetNum) {
+		alert("Game Over");
+	}
+
+	//ive worked on this one and hangman for about 20 hours
+	//... cant get it to work.. sorry
+
+
